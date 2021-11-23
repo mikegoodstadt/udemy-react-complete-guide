598b889ec2337ba5f8b4ee41a141257665e5913f
mikegoodstadt <mikegoodstadt@gmail.com>
2021-11-23 08:33:35 +0100

    Section 3: Chapter 38

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# tut-react-complete-guide
+# Tutorial: React - The Complete Guidetut-react-complete-guide
+[Udemy Course] (https://cognizant.udemy.com/course/react-the-complete-guide-incl-redux)
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/tut-react-complete-guide)
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  return <h2>Expense item!</h2>
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
