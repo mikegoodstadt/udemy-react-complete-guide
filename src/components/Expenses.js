6a20d21f40700dc6d6f25eeebfd468d6a974f3e7
Mike Goodstadt <mike.goodstadt@gmail.com>
2021-11-23 10:47:47 +0100

    Assignment 1

---
+++ src/components/Expenses.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+  <div className="expenses">
+    {props.expenses.map((item) => {
+      return <ExpenseItem
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+        />;
+    })}
+  </div>
+  );
+}
+
+export default Expenses;
